9577c16f6419930f8b2a707b69d8fda24dc9f73d
Elmar Chen <chxiao@gmail.com>
2020-04-21 18:11:18 +0800

    add sample

--- src/main/java/NumberSampler.java
+++ src/main/java/NumberSampler.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class NumberSampler {
-	
+	public static void main(String[] args) throws IOException {
+	    String letter = "[a-zA-Z]";
+    	String word = "[a-zA-Z]{3, 5, 18:exp_normal}%80|[a-z]";
+    	 
+    }
 }
