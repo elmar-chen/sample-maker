52ee97d5572f2faa9232650b5334e0a0aba4fc80
Elmar Chen <chxiao@gmail.com>
2020-04-30 20:05:05 +0800

    aaa

--- src/main/java/elmar/test/sample/maker/parser/ExpressionParserBuilder.java
+++ src/main/java/elmar/test/sample/maker/parser/ExpressionParserBuilder.java
@@ -5,7 +5,7 @@ import elmar.test.sample.maker.parser.composite.Repeat;
 import elmar.test.sample.maker.parser.composite.RepeatParser;
 import elmar.test.sample.maker.parser.composite.SequenceParser;
 
-public class ExpressionParserBuilder extends ParserBuilder{
+public class ExpressionParserBuilder extends ParserBuilder {
     public Parser build() {
 
         return RepeatParser.build(buildParts(), Repeat.ANY_NUMBER);
@@ -15,18 +15,40 @@ public class ExpressionParserBuilder extends ParserBuilder{
         return RepeatParser.build(new PartParserBuilder(), Repeat.ONE_OR_MORE);
     }
 
-    private Parser quotedParts() {
-        return SequenceParser.start()
-                .add(new RegExpLexer("("), Repeat.EXACTLY_ONE)
-                .add(new PartParserBuilder(), Repeat.ONE_OR_MORE)
-                .add(new RegExpLexer(")"), Repeat.EXACTLY_ONE);
-    }
-
     public static class PartParserBuilder extends ParserBuilder {
 
         @Override
         public Parser build() {
-            return null;
+            SequenceParser unQuotedPart = SequenceParser.start()
+                    .add(enumPart(), Repeat.EXACTLY_ONE)
+                    .add(distributionPart(), Repeat.ZERO_OR_ONE);
+            return ChoicesParser.start()
+                    .add(quotedParts())
+                    .add(unQuotedPart);
+        }
+
+        private Parser enumPart() {
+            Parser enumItem = ChoicesParser.start()
+                    .add(new RegExpLexer("<[A-Za-z_ -]+>"))
+                    .add(new RegExpLexer(""));
+            return SequenceParser.start()
+                    .add(new RegExpLexer("["), Repeat.EXACTLY_ONE)
+                    .add(enumItem, Repeat.ONE_OR_MORE)
+                    .add(new RegExpLexer("["), Repeat.EXACTLY_ONE);
+
+        }
+
+        private Parser distributionPart() {
+            return SequenceParser.start()
+                    .add(new RegExpLexer("[0-9]+"), Repeat.EXACTLY_ONE)
+                    .add(new RegExpLexer("%"), Repeat.ZERO_OR_ONE);
+        }
+
+        private Parser quotedParts() {
+            return SequenceParser.start()
+                    .add(new RegExpLexer("("), Repeat.EXACTLY_ONE)
+                    .add(new PartParserBuilder(), Repeat.ONE_OR_MORE)
+                    .add(new RegExpLexer(")"), Repeat.EXACTLY_ONE);
         }
     }
 }
