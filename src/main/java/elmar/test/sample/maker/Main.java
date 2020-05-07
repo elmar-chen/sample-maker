d3eb365e703fdfda19b2431d49914f66dfe422ec
Elmar Chen <chxiao@gmail.com>
2020-05-07 17:37:23 +0800

    aaa

--- src/main/java/elmar/test/sample/maker/Main.java
+++ src/main/java/elmar/test/sample/maker/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         InputStream src = ClassLoader.getSystemResourceAsStream("lang.def");
         ParseContext context = new ParseContext(src);
-        SequenceParser.start().add(new StatementParserBuilder(), Repeat.EXACTLY_ONE).add(new RegExpLexer("\\r\\n|\\n"), Repeat.EXACTLY_ONE);
+        SequenceParser.with(new StatementParserBuilder(), new RegExpLexer("\\r\\n|\\n"));
         System.out.println((char)src.read());
     }
 }
