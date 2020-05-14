d9797519df58c41514e3d531f0e05c9c945734e3
Elmar Chen <chxiao@gmail.com>
2020-05-14 19:19:20 +0800

    aaa

--- src/main/java/elmar/test/sample/maker/Main.java
+++ src/main/java/elmar/test/sample/maker/Main.java
@@ -2,6 +2,7 @@ package elmar.test.sample.maker;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 
@@ -13,9 +14,11 @@ import elmar.test.sample.maker.parser.composite.RepeatParser;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String content = IOUtils.resourceToString("lang.def", Charset.forName("utf-8"));
-        ParseContext context = new ParseContext(content);
-        RepeatParser parser = ParserBuilder.repeat(ParserBuilder.sequence(new StatementParserBuilder(), new RegExpLexer("\\r\\n|\\n")), Repeat.ONE_OR_MORE);
-        parser.parse(context);
+        Pattern compile = Pattern.compile("[1-9]");
+        System.out.println(compile);
+//        String content = IOUtils.resourceToString("lang.def", Charset.forName("utf-8"));
+//        ParseContext context = new ParseContext(content);
+//        RepeatParser parser = ParserBuilder.repeat(ParserBuilder.sequence(new StatementParserBuilder(), new RegExpLexer("\\r\\n|\\n")), Repeat.ONE_OR_MORE);
+//        parser.parse(context);
     }
 }
--- src/main/java/elmar/test/sample/maker/parser/ParserBuilder.java
+++ src/main/java/elmar/test/sample/maker/parser/ParserBuilder.java
@@ -1,6 +1,7 @@
 package elmar.test.sample.maker.parser;
 
 import elmar.test.sample.maker.ParseContext;
+import elmar.test.sample.maker.Statement;
 
 public abstract class ParserBuilder implements Parser{
     public abstract Parser build();
@@ -8,5 +9,22 @@ public abstract class ParserBuilder implements Parser{
     @Override
     public boolean parse(ParseContext context) {
         build().parse(context);
+        return false;
+    }
+    
+    public void add(Class<?> clazz) {
+        
+    }
+    public void list(Class<?> itemClass, String pad) {
+        
+    }
+    public static void main(String[] args) {
+        ParserBuilder builder = ParserBuilder.createFor(Statement[].class);
+        builder.
+    }
+
+    private static ParserBuilder createFor(Class<Statement[]> class1) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
