dac03422b91223ae4d00da83cf39d20ced2a9784
Elmar Chen <chxiao@gmail.com>
2020-05-27 18:59:17 +0800

    sync

--- src/main/java/elmar/test/sample/maker/parser/ParserBuilder.java
+++ src/main/java/elmar/test/sample/maker/parser/ParserBuilder.java
@@ -3,11 +3,19 @@ package elmar.test.sample.maker.parser;
 public abstract class ParserBuilder {
 
     private Class<?> target;
+    private boolean repeat;
 
-    protected ParserBuilder createFor(Class<?> target) {
+    public ParserBuilder createFor(Class<?> target) {
         this.target = target;
         return this;
-    };
+    }
+    
+    public ParserBuilder repeat() {
+        this.repeat = true;
+        return this;
+    }
 
+    
+    
     
 }
